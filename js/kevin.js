89f57b4ab74be4642b27565c7e207c7da751d293
Kevin Fechner <bkevinfechner1989@gmail.com>
2017-02-18 11:33:27 +0100

    add alpha to navbar >50; buggy if <50; add 'xxx-add-height'

--- js/kevin.js
+++ js/kevin.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+	var transparent = "rgba(255,255,255,0)";
 	// Der Button wird mit JavaScript erzeugt und vor dem Ende des body eingebunden.
 	var back_to_top_button = ['<a href="#top" class="back-to-top"><i class="fa fa-arrow-up" aria-hidden="true"></i></a>'].join("");
 	$("body").append(back_to_top_button);
@@ -10,18 +10,33 @@ $(document).ready(function(){
 	// Funktion für das Scroll-Verhalten
 	$(function () {
 		$(window).scroll(function () {
-			if ($(this).scrollTop() > 100) { // Wenn 100 Pixel gescrolled wurde
+			if ($(this).scrollTop() > 100) { 
 				$('.back-to-top').fadeIn();
 			} else {
 				$('.back-to-top').fadeOut();
 			}
 		});
 
-		$('.back-to-top').click(function () { // Klick auf den Button
+		$('.back-to-top').click(function () { 
 			$('body,html').animate({
-				scrollTop: 0
+				scrollTop: 0,
+				// backgroundColor: transparent
 			}, 800);
 			return false;
+		});
+	});
+
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() < 50) {
+				$('nav').animate({
+					backgroundColor: transparent
+				}, 250);
+			} else {
+				$('nav').animate({
+					backgroundColor: '#241F2B',
+				},500);
+			}
 		});
 	});
 
